93dc463d6929b9f19ad46b0e0cbe06cf16cd4232
Cristi <mantapaul20@gmail.com>
2021-05-10 10:11:20 +0300

    commit

--- main.js
+++ main.js
@@ -8,29 +8,52 @@ const navItems = document.querySelectorAll('.menu-item');
 
 
 // Seti Initial State Of Menu
- let showMenu = false;  
+let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-    if(!showMenu) {
-menuBtn.classList.add('close');
-menu.classList.add('show');
-menuNav.classList.add('show');
-wrapper.classList.add('show');
-navItems.forEach(item => item.classList.add('show'));
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        wrapper.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
 
 
-// Set menu state
-showMenu = true;
+        // Set menu state
+        showMenu = true;
     } else {
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         wrapper.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
-        
+
         // Set Menu State
         showMenu = false;
     }
 }
+
+ // Switch Tabs
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+            
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+            
+        })
+        tab.classList.add('active');
+        target.classList.add('active')
+        
+    })
+})
+
+// Fade in out
 
