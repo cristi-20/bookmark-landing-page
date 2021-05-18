8c8e05c0ccd8a84208c912df2a63cf79a1f68cad
Cristi <mantapaul20@gmail.com>
2021-05-18 13:40:47 +0300

    last commit

--- main.js
+++ main.js
@@ -46,14 +46,71 @@ tabs.forEach(tab => {
             
         })
         tabs.forEach(tab => {
-            tab.classList.remove('active')
-            
+            tab.classList.remove('active')  
         })
         tab.classList.add('active');
         target.classList.add('active')
-        
     })
 })
 
-// Fade in out
+// Collapsible Text
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block" || (panel.style.maxHeight))  {
+            panel.style.display = "none"
+            panel.style.maxHeight = null;
+           
+        } else {
+            panel.style.display = "block";
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    })
+}
+
+// Email Validation
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+})
+
+function checkInputs() {
+   const emailValue =  email.value.trim()
+
+   if(emailValue === '') {
+    setErrorFor(email, 'Email cannot be blank');
+} else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'Not a valid email');
+} else {
+    setSuccessFor(email);
+}
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = document.querySelector('small');
+    // add error message inside small tag
+    small.innerText = message;
+    // add error class
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+
+}
